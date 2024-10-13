8a938589e4e60373faabd1b0afecac19d0b10f8c
Shiryoo <steve@DESKTOP-6OJ72PU>
2024-10-13 14:05:36 +0800

    Something shit

---
+++ views/HomePage.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace CarRentalSystem2.Views
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            this.bunifuPanel2 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.bunifuPanel1 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.bunifuPanel5 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.bunifuLabel3 = new Bunifu.UI.WinForms.BunifuLabel();
+            this.lblTotalCustomer = new Bunifu.UI.WinForms.BunifuLabel();
+            this.bunifuPanel4 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.bunifuLabel1 = new Bunifu.UI.WinForms.BunifuLabel();
+            this.lblTotalIncome = new Bunifu.UI.WinForms.BunifuLabel();
+            this.bunifuPanel3 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.lblAvailableCars = new Bunifu.UI.WinForms.BunifuLabel();
+            this.lblCarCount = new Bunifu.UI.WinForms.BunifuLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.bunifuPanel1.SuspendLayout();
+            this.bunifuPanel5.SuspendLayout();
+            this.bunifuPanel4.SuspendLayout();
+            this.bunifuPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuPanel2
+            // 
+            this.bunifuPanel2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.bunifuPanel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel2.BackgroundImage")));
+            this.bunifuPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel2.BorderColor = System.Drawing.Color.Transparent;
+            this.bunifuPanel2.BorderRadius = 15;
+            this.bunifuPanel2.BorderThickness = 1;
+            this.bunifuPanel2.Location = new System.Drawing.Point(12, 219);
+            this.bunifuPanel2.Name = "bunifuPanel2";
+            this.bunifuPanel2.ShowBorders = true;
+            this.bunifuPanel2.Size = new System.Drawing.Size(956, 489);
+            this.bunifuPanel2.TabIndex = 1;
+            // 
+            // bunifuPanel1
+            // 
+            this.bunifuPanel1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.bunifuPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel1.BackgroundImage")));
+            this.bunifuPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel1.BorderColor = System.Drawing.Color.Transparent;
+            this.bunifuPanel1.BorderRadius = 15;
+            this.bunifuPanel1.BorderThickness = 1;
+            this.bunifuPanel1.Controls.Add(this.bunifuPanel5);
+            this.bunifuPanel1.Controls.Add(this.bunifuPanel4);
+            this.bunifuPanel1.Controls.Add(this.bunifuPanel3);
+            this.bunifuPanel1.Location = new System.Drawing.Point(12, 12);
+            this.bunifuPanel1.Name = "bunifuPanel1";
+            this.bunifuPanel1.ShowBorders = true;
+            this.bunifuPanel1.Size = new System.Drawing.Size(956, 191);
+            this.bunifuPanel1.TabIndex = 0;
+            // 
+            // bunifuPanel5
+            // 
+            this.bunifuPanel5.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.bunifuPanel5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel5.BackgroundImage")));
+            this.bunifuPanel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel5.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.bunifuPanel5.BorderRadius = 8;
+            this.bunifuPanel5.BorderThickness = 2;
+            this.bunifuPanel5.Controls.Add(this.pictureBox3);
+            this.bunifuPanel5.Controls.Add(this.bunifuLabel3);
+            this.bunifuPanel5.Controls.Add(this.lblTotalCustomer);
+            this.bunifuPanel5.Location = new System.Drawing.Point(647, 26);
+            this.bunifuPanel5.Name = "bunifuPanel5";
+            this.bunifuPanel5.ShowBorders = true;
+            this.bunifuPanel5.Size = new System.Drawing.Size(272, 129);
+            this.bunifuPanel5.TabIndex = 4;
+            // 
+            // bunifuLabel3
+            // 
+            this.bunifuLabel3.AllowParentOverrides = false;
+            this.bunifuLabel3.AutoEllipsis = false;
+            this.bunifuLabel3.CursorType = null;
+            this.bunifuLabel3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuLabel3.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.bunifuLabel3.Location = new System.Drawing.Point(107, 35);
+            this.bunifuLabel3.Name = "bunifuLabel3";
+            this.bunifuLabel3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.bunifuLabel3.Size = new System.Drawing.Size(142, 28);
+            this.bunifuLabel3.TabIndex = 2;
+            this.bunifuLabel3.Text = "Total Customer :";
+            this.bunifuLabel3.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.bunifuLabel3.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // lblTotalCustomer
+            // 
+            this.lblTotalCustomer.AllowParentOverrides = false;
+            this.lblTotalCustomer.AutoEllipsis = false;
+            this.lblTotalCustomer.CursorType = null;
+            this.lblTotalCustomer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalCustomer.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblTotalCustomer.Location = new System.Drawing.Point(156, 67);
+            this.lblTotalCustomer.Name = "lblTotalCustomer";
+            this.lblTotalCustomer.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblTotalCustomer.Size = new System.Drawing.Size(11, 28);
+            this.lblTotalCustomer.TabIndex = 0;
+            this.lblTotalCustomer.Text = "9";
+            this.lblTotalCustomer.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.lblTotalCustomer.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // bunifuPanel4
+            // 
+            this.bunifuPanel4.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.bunifuPanel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel4.BackgroundImage")));
+            this.bunifuPanel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel4.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.bunifuPanel4.BorderRadius = 8;
+            this.bunifuPanel4.BorderThickness = 2;
+            this.bunifuPanel4.Controls.Add(this.pictureBox2);
+            this.bunifuPanel4.Controls.Add(this.bunifuLabel1);
+            this.bunifuPanel4.Controls.Add(this.lblTotalIncome);
+            this.bunifuPanel4.Location = new System.Drawing.Point(335, 26);
+            this.bunifuPanel4.Name = "bunifuPanel4";
+            this.bunifuPanel4.ShowBorders = true;
+            this.bunifuPanel4.Size = new System.Drawing.Size(272, 129);
+            this.bunifuPanel4.TabIndex = 3;
+            // 
+            // bunifuLabel1
+            // 
+            this.bunifuLabel1.AllowParentOverrides = false;
+            this.bunifuLabel1.AutoEllipsis = false;
+            this.bunifuLabel1.CursorType = null;
+            this.bunifuLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuLabel1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.bunifuLabel1.Location = new System.Drawing.Point(113, 33);
+            this.bunifuLabel1.Name = "bunifuLabel1";
+            this.bunifuLabel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.bunifuLabel1.Size = new System.Drawing.Size(122, 28);
+            this.bunifuLabel1.TabIndex = 2;
+            this.bunifuLabel1.Text = "Total Income :";
+            this.bunifuLabel1.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.bunifuLabel1.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // lblTotalIncome
+            // 
+            this.lblTotalIncome.AllowParentOverrides = false;
+            this.lblTotalIncome.AutoEllipsis = false;
+            this.lblTotalIncome.CursorType = null;
+            this.lblTotalIncome.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalIncome.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblTotalIncome.Location = new System.Drawing.Point(140, 67);
+            this.lblTotalIncome.Name = "lblTotalIncome";
+            this.lblTotalIncome.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblTotalIncome.Size = new System.Drawing.Size(70, 28);
+            this.lblTotalIncome.TabIndex = 0;
+            this.lblTotalIncome.Text = "$11,000";
+            this.lblTotalIncome.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.lblTotalIncome.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // bunifuPanel3
+            // 
+            this.bunifuPanel3.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.bunifuPanel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel3.BackgroundImage")));
+            this.bunifuPanel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel3.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.bunifuPanel3.BorderRadius = 8;
+            this.bunifuPanel3.BorderThickness = 2;
+            this.bunifuPanel3.Controls.Add(this.pictureBox1);
+            this.bunifuPanel3.Controls.Add(this.lblAvailableCars);
+            this.bunifuPanel3.Controls.Add(this.lblCarCount);
+            this.bunifuPanel3.Location = new System.Drawing.Point(25, 26);
+            this.bunifuPanel3.Name = "bunifuPanel3";
+            this.bunifuPanel3.ShowBorders = true;
+            this.bunifuPanel3.Size = new System.Drawing.Size(272, 129);
+            this.bunifuPanel3.TabIndex = 0;
+            // 
+            // lblAvailableCars
+            // 
+            this.lblAvailableCars.AllowParentOverrides = false;
+            this.lblAvailableCars.AutoEllipsis = false;
+            this.lblAvailableCars.CursorType = null;
+            this.lblAvailableCars.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAvailableCars.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblAvailableCars.Location = new System.Drawing.Point(106, 33);
+            this.lblAvailableCars.Name = "lblAvailableCars";
+            this.lblAvailableCars.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblAvailableCars.Size = new System.Drawing.Size(131, 28);
+            this.lblAvailableCars.TabIndex = 2;
+            this.lblAvailableCars.Text = "Available Cars :";
+            this.lblAvailableCars.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.lblAvailableCars.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // lblCarCount
+            // 
+            this.lblCarCount.AllowParentOverrides = false;
+            this.lblCarCount.AutoEllipsis = false;
+            this.lblCarCount.CursorType = null;
+            this.lblCarCount.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCarCount.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblCarCount.Location = new System.Drawing.Point(163, 67);
+            this.lblCarCount.Name = "lblCarCount";
+            this.lblCarCount.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblCarCount.Size = new System.Drawing.Size(11, 28);
+            this.lblCarCount.TabIndex = 0;
+            this.lblCarCount.Text = "1";
+            this.lblCarCount.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.lblCarCount.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.pictureBox1.BackgroundImage = global::CarRentalSystem2.Properties.Resources.car2;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(73, 62);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.pictureBox2.BackgroundImage = global::CarRentalSystem2.Properties.Resources.money;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(16, 33);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(73, 62);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.pictureBox3.BackgroundImage = global::CarRentalSystem2.Properties.Resources.people;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(18, 33);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(73, 70);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(980, 720);
+            this.Controls.Add(this.bunifuPanel2);
+            this.Controls.Add(this.bunifuPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "HomePage";
+            this.Text = "HomePage";
+            this.bunifuPanel1.ResumeLayout(false);
+            this.bunifuPanel5.ResumeLayout(false);
+            this.bunifuPanel5.PerformLayout();
+            this.bunifuPanel4.ResumeLayout(false);
+            this.bunifuPanel4.PerformLayout();
+            this.bunifuPanel3.ResumeLayout(false);
+            this.bunifuPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel1;
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel3;
+        private Bunifu.UI.WinForms.BunifuLabel lblAvailableCars;
+        private Bunifu.UI.WinForms.BunifuLabel lblCarCount;
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel2;
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel5;
+        private Bunifu.UI.WinForms.BunifuLabel bunifuLabel3;
+        private Bunifu.UI.WinForms.BunifuLabel lblTotalCustomer;
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel4;
+        private Bunifu.UI.WinForms.BunifuLabel bunifuLabel1;
+        private Bunifu.UI.WinForms.BunifuLabel lblTotalIncome;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
