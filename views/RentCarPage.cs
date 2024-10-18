78826f0d1f711f41fbd6c7be9be2f96991892176
balinojanpaul <balinojanpaul@gmail.com>
2024-10-18 23:14:41 +0800

    Revert "Fixed the overlapping datagrid view (Steven's Fault)"
    This reverts commit 72d413e1062cad60b89d2b8805d64aba574ab891.

--- views/RentCarPage.cs
+++ views/RentCarPage.cs
@@ -259,7 +259,8 @@ namespace CarRentalSystem2.Views
             // Optional: Set the grid's border style
             dtgAvailableCars.BorderStyle = BorderStyle.None;
 
-            
+            // Set the dock style to fill
+            dtgAvailableCars.Dock = DockStyle.Fill;
 
             // Set row styling
             dtgAvailableCars.RowsDefaultCellStyle.BackColor = Color.White; // White background for rows
